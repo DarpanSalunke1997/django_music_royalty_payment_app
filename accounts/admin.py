6ccc4279763f21440b204bd7006059b782fcb4ae
Logicrays <darpansalunk@gmail.com>
2021-08-26 13:17:42 +0530

    Update 2.0

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Kineto_Kisom, Profile
+from .models import Kineto_Kisom, Profile, RBT_MAR, AltaFonte_MAR
 from import_export.admin import ImportExportModelAdmin
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
@@ -22,14 +22,18 @@ class CustomUserAdmin(UserAdmin):
             return list()
         return super(CustomUserAdmin, self).get_inline_instances(request, obj)
 
-class Kineto_KisomAdmin(ImportExportModelAdmin):
+class Kineto_KisomDataAdmin(ImportExportModelAdmin):
     pass
-
+class RBT_MARDataAdmin(ImportExportModelAdmin):
+    pass
+class AltaFonte_MARDataAdmin(ImportExportModelAdmin):
+    pass
 # Register your models here.
 admin.site.unregister(User)
 admin.site.unregister(Group)
-# admin.site.unregister(TokenProxy)
 admin.site.register(User, CustomUserAdmin)
 
 
-admin.site.register(Kineto_Kisom, Kineto_KisomAdmin, list_display = ('artistname','trackname','net_royalty_total'))
+admin.site.register(Kineto_Kisom, Kineto_KisomDataAdmin, list_display = ('ARTIST_NAME','CONTENT_NAME','NET_ROYALTY_TOTAL'))
+admin.site.register(RBT_MAR, RBT_MARDataAdmin, list_display = ('ARTIST_NAME','CONTENT_NAME','REV_X_2'))
+admin.site.register(AltaFonte_MAR, AltaFonte_MARDataAdmin, list_display = ('SERVICE_ID','COUNTRY','ARTIST_DISPLAY','TRACK_TITLE','NET'))
--- accounts/models.py
+++ accounts/models.py
@@ -42,10 +42,28 @@ class Profile(models.Model):
 
 
 class Kineto_Kisom(models.Model):
-    artistname = models.CharField(default="",max_length=100,blank=True, null=True)
-    trackname = models.CharField(default="",max_length=100,blank=True, null=True)
-    net_royalty_total = models.FloatField(default=0.0, null=True,blank=True)
+    ARTIST_NAME = models.CharField(default="",max_length=100,blank=True, null=True)
+    CONTENT_NAME = models.CharField(default="",max_length=100,blank=True, null=True)
+    NET_ROYALTY_TOTAL = models.FloatField(default=0.0, null=True,blank=True)
 
     def __str__(self):
-        return str(self.artistname)
-    
+        return str(self.ARTIST_NAME)
+
+class RBT_MAR(models.Model):
+    ARTIST_NAME = models.CharField(default="",max_length=100,blank=True, null=True)
+    CONTENT_NAME = models.CharField(default="",max_length=100,blank=True, null=True)
+    REV_X_2 = models.FloatField(default=0.0, null=True,blank=True)
+
+    def __str__(self):
+        return str(self.ARTIST_NAME)
+
+class AltaFonte_MAR(models.Model):
+    SERVICE_ID = models.CharField(default="",max_length=100,blank=True, null=True)
+    COUNTRY = models.CharField(default="",max_length=200,blank=True, null=True)
+    ARTIST_DISPLAY = models.CharField(default="",max_length=500,blank=True, null=True)
+    TRACK_TITLE = models.CharField(default="",max_length=300,blank=True, null=True)
+    NET = models.FloatField(default=0.0, null=True,blank=True)
+
+    def __str__(self):
+        return str(self.ARTIST_DISPLAY)
+
